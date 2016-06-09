0d30ecd9fbf153dec1db5ab609e2ef141e8aafdd
mixeych <dmitriy_miheev@mail.ru>
2016-06-09 10:58:39 +0300

    validation

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -7,14 +7,43 @@ socket.on("chat", function (data){
 $(document).ready(function(){
     $("form.form-horizontal").submit(function(e){
         e.preventDefault();
+        var text = $("#enter-message").val();
+        var email = $("#enter-email").val();
+        var name = $("#enter-name").val();
+        var homepage = $("#enter-homepage").val();
+        var notValid = false;
+        if(!text){
+            $("#enter-message").css("background-color", "rgb(234, 110, 110)");
+            notValid = true;
+        }else{
+            text.replace(/(<([^>]+)>)/ig,"");
+            $("#enter-message").css("background-color", "rgb(255, 255, 255)");
+        }
+        var regExp = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+        if(!email||!regExp.test(email)){
+            $("#enter-email").css("background-color", "rgb(234, 110, 110)");
+            notValid = true;
+        }else{
+            $("#enter-email").css("background-color", "rgb(255, 255, 255)");
+        }
+        if(!name){
+            $("#enter-name").css("background-color", "rgb(234, 110, 110)");
+            notValid = true;
+        }else{
+            $("#enter-name").css("background-color", "rgb(255, 255, 255)");
+        }
+        if(notValid){
+            return false;
+        }
+
         var message = {
-            text: $("#enter-message").val(),
-            email: $("#enter-email").val(),
-            name: $("#enter-name").val(),
-            homepage: $("#enter-homepage").val(),
+            text: text,
+            email: email,
+            name: name,
+            homepage: homepage,
         }
         socket.emit("message", message, function(data){
-            var row = "<tr><td>"+message.name+"</td><td>"+message.email+"</td><td>"+message.homepage+"</td><td>"+message.text+"</td></tr>";
+            var row = "<tr><td>"+data.name+"</td><td>"+data.email+"</td><td>"+data.homepage+"</td><td>"+data.text+"</td></tr>";
             $("#chat tbody").prepend(row);
         });
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var connection = require('../libs/mysqldb');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var messages = connection.query('SELECT * FROM message ORDER BY created_at ASC LIMIT 5', function(error, results, field){
+  var messages = connection.query('SELECT * FROM message ORDER BY created_at DESC LIMIT 5', function(error, results, field){
       if(error){
         return next(error);
       }
