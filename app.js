446b78116a42a680689ea4a5b5df0ee70aa29f38
mixeych <dmitriy_miheev@mail.ru>
2016-06-09 18:01:58 +0300

    validation

--- app.js
+++ app.js
@@ -21,8 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+var paginate = require('express-paginate');
+app.use(paginate.middleware(5, 10));
 app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
